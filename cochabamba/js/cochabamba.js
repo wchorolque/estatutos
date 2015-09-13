12b5ec9111d35178975458b42e20ce59d7c7a374
w. chorolque <wchorolque@gmail.com>
2015-09-13 02:03:16 -0400

    agregado archivos para visualizar el estatuto de cochabamba

---
+++ cochabamba/js/cochabamba.js
@@ -0,0 +1,11 @@
+var csv_filename = "cochabamba.csv";
+var etiquetas = {
+    "Gobierno Aut\u00f3nomo Departamental": "#5687d1",
+    "Organizaci\u00f3n del Gobierno Aut\u00f3nomo Departamental": "#D20001",
+    "Relaciones Intergubernativas e Interinstitucionales": "#F79E37",
+    "Planificaci\u00f3n y Organizaci\u00f3n Administrativa y Financiera": "#FE6612",
+    "Pol\u00edticas para Vivir Bien en la Llaqta": "#a173d1",
+    "Ordenamiento Jur\u00eddico y Reforma del Estatuto Auton\u00f3mico": "#E80C7A",
+    "Art\u00edculos": "#22B266"
+};
+
