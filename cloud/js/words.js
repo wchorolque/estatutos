2b49ec31323d18b5caf1b22877266e9fac48a3f9
w. chorolque <wchorolque@gmail.com>
2015-09-13 22:11:32 -0400

    Completado comparación de estatutos

--- cloud/js/words.js
+++ cloud/js/words.js
@@ -1,8 +1,8 @@
 var cloud_words = [];
 
-var contar_palabras = function(data) {
-    data.forEach(function (d) {
-        var tags = d.tags.split(" ");
+var contar_palabras = function (data) {
+    data.forEach(function (record) {
+        var tags = record.tags.split(" ");
         tags.forEach(function (d) {
             var r = cloud_words.filter(function (tag) {
                 return tag.text == d;
@@ -11,17 +11,59 @@ var contar_palabras = function(data) {
                 cloud_words.push({
                         text: d,
                         weight: 1,
-                        link: '#'
+                        handlers: {
+                            click: function () {
+                                mostrar_articulos(d);
+                            }
+                        }
                     }
                 );
             } else {
-                cloud_words.forEach(function(tag) {
+                cloud_words.forEach(function (tag) {
                     if (tag.text === d) {
                         tag.weight += 1;
                     }
                 })
             }
         })
+    });
+};
+
+mostrar_articulos = function (word) {
+    d3.json('../../data/estatuto_oruro', function (data) {
+        buscar_articulos(data, word, 'div#oruro', '../../oruro/articulos/');
+    });
+    d3.json('../../data/estatuto_lapaz', function (data) {
+        buscar_articulos(data, word, 'div#lapaz', '../../lapaz/articulos/');
+    });
+    d3.json('../../data/estatuto_potosi', function (data) {
+        buscar_articulos(data, word, 'div#potosi', '../../potosi/articulos/');
+    });
+    d3.json('../../data/estatuto_cochabamba', function (data) {
+        buscar_articulos(data, word, 'div#cochabamba', '../../cochabamba/articulos/');
+    });
+};
+
+buscar_articulos = function(data, word, target, articulos) {
+    d3.select(target).html("");
+    data.forEach(function (d) {
+        var tags = d.tags.split(" ");
+        tags.forEach(function (word_tag) {
+            if (word == word_tag) {
+                var prefix = "";
+                if (d.numero_articulo > 0 && d.numero_articulo < 10) {
+                    prefix = "00";
+                } else if (d.numero_articulo >= 10 && d.numero_articulo < 100) {
+                    prefix = "0";
+                }
+                console.log(target + ":" + d.numero_articulo);
+                d3.text(articulos + prefix + d.numero_articulo + '.html', function (error, data) {
+                    if (error === null) {
+                        $(target).append(data);
+                    }
+                });
+            }
+        });
     });
 };
 
@@ -34,13 +76,7 @@ function analyze(error, oruro, potosi, cochabamba, lapaz) {
     contar_palabras(cochabamba);
     contar_palabras(lapaz);
 
-    cloud_words.forEach(function(d) {
-        console.log(d.text);
-    });
-
-    console.log(cloud_words.length);
-
-    $('#palabras').jQCloud(cloud_words, { shape: 'rectangular'});
+    $('#palabras').jQCloud(cloud_words, {shape: 'rectangular'});
 };
 
 $(document).ready(function (event) {
