bf73a6411388600cf210f0d14c39a950e8d8efbb
Ariel Canaviri Torrico <ariel.canaviri@gmail.com>
2015-09-14 00:46:45 -0400

    agregados los acordeones en las nubes de  palabras

---
+++ cloud/js/estilo.js
@@ -0,0 +1,20 @@
+$(function(){
+
+	var changed=$("#oruro, #lapaz, #potosi, #cochabamba");
+	changed.on('DOMSubtreeModified',function(e){
+		$('.block').hide();
+		$('.accordion .titulo').on('click',function(){
+			// if($(this).next().is(':visible')){
+			// 	$(this).next().slideUp();
+			// }
+			if($(this).next().is(':hidden')){
+				$('.accordion .titulo').next().slideUp();
+				$(this).next().slideDown();
+			}
+		});
+		$("#oruro_titulo").text($("#oruro").find('article').length);
+		$("#lapaz_titulo").text($("#lapaz").find('article').length);
+		$("#potosi_titulo").text($("#potosi").find('article').length);
+		$("#cochabamba_titulo").text($("#cochabamba").find('article').length);
+	});
+});
